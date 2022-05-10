ee51eed8a585c023443090c8b810b7fe6aa24ea6
klmpk <105263770+klmpk@users.noreply.github.com>
2022-05-11 05:31:24 +0700

    tugas web

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# klmpk5.github.io
-kelompok 5
+![Doge1](https://c.tenor.com/XUX6DFHZ-l0AAAAj/cool-doge-cool-dog.gif)
+![Doge2](https://c.tenor.com/XUX6DFHZ-l0AAAAj/cool-doge-cool-dog.gif)
+![Doge3](https://c.tenor.com/XUX6DFHZ-l0AAAAj/cool-doge-cool-dog.gif)
+![Doge4](https://c.tenor.com/XUX6DFHZ-l0AAAAj/cool-doge-cool-dog.gif)
+
+```
+Made just for fun and to learn more about front-end skills
+```
+
---
+++ assets/js/ref.js
@@ -0,0 +1,41 @@
+const options = {
+  strings: ["Dosen Pengampu ", "R. Arum, S.P.,S.SI., M.T.", "Trisya Septiana, S.T., M.T."],
+  startDelay:1000,
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+};
+
+const typed = new Typed(".typed", options);
+new fullpage("#fullpage", {
+  navigation: true,
+  scrollingSpeed: 1000,
+  navigationTooltips: ["Home"],
+});
+
+const themeDots = document.getElementsByClassName("theme-dot");
+for (let i = 0; i < themeDots.length; i++) {
+  themeDots[i].addEventListener("click", function () {
+    const mode = this.dataset.mode;
+    setTheme(mode)
+  })
+}
+
+function setTheme(mode) {
+  if (mode === "dark-blue") {
+    document.getElementById("theme-style").href = "assets/css/style.css"
+  }
+  if (mode === "dark-red") {
+    document.getElementById("theme-style").href = "assets/css/theme/dark-red.css"
+  }
+  if (mode === "green") {
+    document.getElementById("theme-style").href = "assets/css/theme/green.css"
+  }
+  if (mode === "soft-red") {
+    document.getElementById("theme-style").href = "assets/css/theme/soft-red.css"
+  }
+  if (mode === "violet") {
+    document.getElementById("theme-style").href = "assets/css/theme/violet.css"
+  }
+}
